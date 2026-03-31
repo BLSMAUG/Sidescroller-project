c6c65de2104ed4dd5c0c781a870905db3fa78f6b
BLSMAUG <148821638+BLSMAUG@users.noreply.github.com>
2026-03-31 12:15:08 +0200

    Add files via upload

---
+++ Assets/Scripts/CameraGround.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraGround : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float smoothSpeed = 0.3f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(player.transform.position.x + 6, player.transform.position.y + 3, -10);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
